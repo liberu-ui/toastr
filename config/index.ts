8ca49799c558ca23f14dbbaede34d5c8a371f19a
Curtis Delicata <curtis.delicata@outlook.com>
2023-10-07 22:47:32 +0000

    Fix

---
+++ config/index.ts
@@ -0,0 +1,5 @@
+import { IconType } from '../src/config/icons';
+import { Type } from '../src/config/types';
+import { Position } from '../src/config/positions';
+
+export { IconType, Type, Position };
